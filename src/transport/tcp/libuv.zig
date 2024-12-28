92ec0ba39a10c6c30c1ae5b3de20b3b2781e913e
Chen Kai <281165273grape@gmail.com>
2024-12-28 23:12:37 +0800

    add multiaddr to sa helper
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/transport/tcp/libuv.zig
+++ src/transport/tcp/libuv.zig
@@ -27,4 +27,41 @@ pub const LibuvProvider = struct {
         self.tcp.Bind(&sa, false) catch unreachable;
         self.tcp.GetStream().Listen(128, null) catch unreachable;
     }
+
+    fn multiaddr_to_sockaddr(addr: *Multiaddr) !uv.SocketAddress {
+        var port: ?u16 = null;
+        var address = addr.*;
+
+        while (try address.pop()) |proto| {
+            switch (proto) {
+                .Ip4 => |ipv4| {
+                    if (port) |p| {
+                        const bytes = @as(*const [4]u8, @ptrCast(ipv4.sa.addr));
+                        const sockaddr = try uv.SocketAddress.FromString(bytes, p);
+                        return sockaddr;
+                    } else {
+                        return error.MissingPort;
+                    }
+                },
+                .Ip6 => |ipv6| {
+                    if (port) |p| {
+                        const bytes = @as(*const [16]u8, @ptrCast(ipv6.sa.addr));
+                        const sockaddr = try uv.SocketAddress.FromString(bytes, p);
+                        return sockaddr;
+                    } else {
+                        return error.MissingPort;
+                    }
+                },
+                .Tcp => |portnum| {
+                    if (port != null) {
+                        return error.DuplicatePort;
+                    }
+                    port = portnum;
+                },
+                .P2p => {},
+                else => return error.UnsupportedProtocol,
+            }
+        }
+        return error.InvalidAddress;
+    }
 };
