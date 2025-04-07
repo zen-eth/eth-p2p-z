32e15ef683580d2105f5de3171e0c59f459c5e8b
Chen Kai <281165273grape@gmail.com>
2025-04-07 20:26:04 +0800

    revert mutex
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/connection/conn.zig
+++ src/connection/conn.zig
@@ -294,8 +294,13 @@ test "GenericConn with std.net.Stream" {
     client_stream.close();
 }
 
+var pipe_mutex: std.Thread.Mutex = .{};
+
 /// Creates a pair of connected PipeConn instances
 pub fn createPipeConnPair() !struct { client: PipeConn, server: PipeConn } {
+    pipe_mutex.lock();
+    defer pipe_mutex.unlock();
+
     const fds1 = try posix.pipe();
     errdefer {
         posix.close(fds1[0]);
