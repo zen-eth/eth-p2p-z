cdcae807cb4441feff116fa1afbbe1ebb7e43a83
Chen Kai <281165273grape@gmail.com>
2025-05-23 23:36:05 +0800

    feat:multistream negotiation
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/multistream/lib.zig
+++ src/multistream/lib.zig
@@ -4,6 +4,7 @@ const testing = std.testing;
 pub const proto_matcher = @import("protocol_matcher.zig");
 pub const proto_desc = @import("protocol_descriptor.zig");
 pub const proto_binding = @import("protocol_binding.zig");
+pub const multistream = @import("multistream.zig");
 
 test {
     std.testing.refAllDeclsRecursive(@This());
--- src/multistream/multistream.zig
+++ src/multistream/multistream.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const proto_binding = @import("protocol_binding.zig");
 const ArrayList = std.ArrayList;
 const p2p_conn = @import("../conn.zig");
+const ProtocolId = @import("../protocol_id.zig").ProtocolId;
+const multiformats = @import("multiformats");
+const uvarint = multiformats.uvarint;
 
 pub const Multistream = struct {
     bindings: ArrayList(proto_binding.AnyProtocolBinding),
@@ -20,46 +23,96 @@ pub const Multistream = struct {
     }
 };
 
-// package io.libp2p.multistream
+pub const Negotiator = struct {
+    const MULTISTREAM_PROTO = "/multistream/1.0.0";
+    const MESSAGE_SUFFIX = "\n";
+    const NA = "na";
+    const LS = "ls";
 
-// import io.libp2p.core.P2PChannel
-// import io.libp2p.core.P2PChannelHandler
-// import io.libp2p.core.multistream.Multistream
-// import io.libp2p.core.multistream.ProtocolBinding
-// import java.time.Duration
-// import java.util.concurrent.CompletableFuture
+    const MAX_MULTISTREAM_MESSAGE_LENGTH = 1024;
+    const MESSAGE_SUFFIX_LENGTH = MESSAGE_SUFFIX.len;
+    const MAX_PROTOCOL_ID_LENGTH = MAX_MULTISTREAM_MESSAGE_LENGTH - MESSAGE_SUFFIX_LENGTH;
+    const UVARINT16_MAX_LENGTH = 3;
 
-// class MultistreamImpl<TController>(
-//     override val bindings: List<ProtocolBinding<TController>>,
-//     val preHandler: P2PChannelHandler<*>? = null,
-//     val postHandler: P2PChannelHandler<*>? = null,
-//     val negotiationTimeLimit: Duration = DEFAULT_NEGOTIATION_TIME_LIMIT
-// ) : Multistream<TController> {
+    bindings: ArrayList(ProtocolId),
 
-//     override fun initChannel(ch: P2PChannel): CompletableFuture<TController> {
-//         return with(ch) {
-//             preHandler?.also {
-//                 it.initChannel(ch)
-//             }
-//             pushHandler(
-//                 if (ch.isInitiator) {
-//                     Negotiator.createRequesterInitializer(
-//                         negotiationTimeLimit,
-//                         *bindings.flatMap { it.protocolDescriptor.announceProtocols }.toTypedArray()
-//                     )
-//                 } else {
-//                     Negotiator.createResponderInitializer(
-//                         negotiationTimeLimit,
-//                         bindings.map { it.protocolDescriptor.protocolMatcher }
-//                     )
-//                 }
-//             )
-//             postHandler?.also {
-//                 it.initChannel(ch)
-//             }
-//             val protocolSelect = ProtocolSelect(bindings)
-//             pushHandler(protocolSelect)
-//             protocolSelect.selectedFuture
-//         }
-//     }
-// }
+    negotiation_time_limit: u64,
+
+    message_pool: std.heap.MemoryPool([UVARINT16_MAX_LENGTH + MAX_MULTISTREAM_MESSAGE_LENGTH]u8),
+
+    allocator: std.mem.Allocator,
+
+    const Self = @This();
+
+    // --- Actual Handler Implementations ---
+    pub fn onActiveImpl(self: *Self, ctx: *p2p_conn.HandlerContext) void {
+        const WriteCallbackContext = struct {
+            negotiator: *Self,
+            buffer: []u8,
+        };
+
+        if (ctx.conn.direction() == p2p_conn.Direction.OUTBOUND) {
+            // Initiator
+            const buffer = self.allocator.alloc(u8, (UVARINT16_MAX_LENGTH + MAX_MULTISTREAM_MESSAGE_LENGTH) * 2) catch unreachable;
+            var proto_buffer = std.io.fixedBufferStream(buffer);
+            var proto_writer = proto_buffer.writer();
+
+            _ = uvarint.encodeStream(proto_writer, u8, MULTISTREAM_PROTO.len) catch unreachable;
+
+            proto_writer.writeAll(MULTISTREAM_PROTO) catch unreachable;
+            proto_writer.writeAll(MESSAGE_SUFFIX) catch unreachable;
+
+            _ = uvarint.encodeStream(proto_writer, u16, @intCast(self.bindings.items[0].len)) catch unreachable;
+            proto_writer.writeAll(self.bindings.items[0]) catch unreachable;
+            proto_writer.writeAll(MESSAGE_SUFFIX) catch unreachable;
+
+            const callback_ctx = self.allocator.create(WriteCallbackContext) catch unreachable;
+            callback_ctx.* = .{
+                .negotiator = self,
+                .buffer = buffer,
+            };
+
+            ctx.write(proto_buffer.getWritten(), callback_ctx, struct {
+                pub fn callback(w: ?*anyopaque, _: anyerror!usize) void {
+                    const w_ctx: *WriteCallbackContext = @ptrCast(@alignCast(w.?));
+                    const allocator = w_ctx.negotiator.allocator;
+                    allocator.free(w_ctx.buffer);
+                    allocator.destroy(w_ctx);
+                }
+            }.callback);
+        } else {
+            // Responder
+            const buffer = self.message_pool.create() catch unreachable;
+            var proto_buffer = std.io.fixedBufferStream(buffer);
+            var proto_writer = proto_buffer.writer();
+
+            _ = uvarint.encodeStream(proto_writer, u8, MULTISTREAM_PROTO.len) catch unreachable;
+
+            proto_writer.writeAll(MULTISTREAM_PROTO) catch unreachable;
+            proto_writer.writeAll(MESSAGE_SUFFIX) catch unreachable;
+
+            const callback_ctx = self.allocator.create(WriteCallbackContext) catch unreachable;
+            callback_ctx.* = .{
+                .negotiator = self,
+                .buffer = buffer,
+            };
+            ctx.write(proto_buffer.getWritten(), null, struct {
+                pub fn callback(w: ?*anyopaque, _: anyerror!usize) void {
+                    const w_ctx: *WriteCallbackContext = @ptrCast(@alignCast(w.?));
+                    const allocator = w_ctx.negotiator.allocator;
+                    w_ctx.negotiator.message_pool.destroy(
+                        @alignCast(
+                            @as(*[UVARINT16_MAX_LENGTH + MAX_MULTISTREAM_MESSAGE_LENGTH]u8, @ptrFromInt(@intFromPtr(w_ctx.buffer.ptr))),
+                        ),
+                    );
+                    allocator.destroy(w_ctx);
+                }
+            }.callback);
+        }
+        ctx.fireActive();
+    }
+
+    pub fn onRead(_: *Self, _: p2p_conn.AnyRxConn, _: []const u8, _: usize, _: *const fn (ud: ?*anyopaque, r: anyerror!*anyopaque) void) void {
+        // Handle incoming messages
+    }
+};
