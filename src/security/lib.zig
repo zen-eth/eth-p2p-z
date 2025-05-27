2b6965edeb69c6a9db9566e9e214d6f7089a3a21
Chen Kai <281165273grape@gmail.com>
2025-05-27 23:24:06 +0800

    fix test error
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/security/lib.zig
+++ src/security/lib.zig
@@ -3,7 +3,7 @@ const testing = std.testing;
 
 pub const session = @import("session.zig");
 pub const plain = @import("plain.zig");
-pub const mock_security = @import("mock_security.zig");
+pub const insecure = @import("insecure.zig");
 
 test {
     std.testing.refAllDeclsRecursive(@This());
