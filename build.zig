9651cb55bac54897bb80273d91ff12b799c86fd5
Chen Kai <281165273grape@gmail.com>
2025-07-25 22:41:12 +0800

    revert:revert uncorrect ci fix
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- build.zig
+++ build.zig
@@ -9,14 +9,8 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    var target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{});
 
-    if (target.result.os.tag == .linux) {
-        target.result.cpu.model = std.Target.Cpu.Model.baseline(
-            target.result.cpu.arch,
-            target.result.os,
-        );
-    }
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
