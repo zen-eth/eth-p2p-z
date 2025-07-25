90e4f0f3ae8c3690da9de5d6564f43c0f0933168
Chen Kai <281165273grape@gmail.com>
2025-07-25 16:37:04 +0800

    feat:add baseline build option
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- build.zig
+++ build.zig
@@ -9,8 +9,14 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    var target = b.standardTargetOptions(.{});
 
+    if (target.result.os.tag == .linux) {
+        target.result.cpu.model = std.Target.Cpu.Model.baseline(
+            target.result.cpu.arch,
+            target.result.os,
+        );
+    }
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
