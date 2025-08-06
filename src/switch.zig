43ccf718ca85e1ffb5e44c734ee6077b8c8aa1f9
Chen Kai <281165273grape@gmail.com>
2025-08-06 21:46:41 +0800

    test multistream
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/switch.zig
+++ src/switch.zig
@@ -28,6 +28,8 @@ pub const Switch = struct {
 
     incoming_connections: std.ArrayList(*quic.QuicConnection),
 
+    is_stopping: bool,
+
     pub fn init(self: *Switch, allocator: Allocator, transport: *quic.QuicTransport) void {
         self.* = Switch{
             .proto_handlers = std.ArrayList(protocols.AnyProtocolHandler).init(allocator),
@@ -36,71 +38,14 @@ pub const Switch = struct {
             .allocator = allocator,
             .listeners = std.StringArrayHashMap(quic.QuicListener).init(allocator),
             .incoming_connections = std.ArrayList(*quic.QuicConnection).init(allocator),
+            .is_stopping = false,
         };
     }
 
     pub fn deinit(self: *Switch) void {
-        // Close all outgoing connections. The onOutgoingConnectionClose callback
-        // will be triggered for each, handling removal and cleanup.
-        var out_iter = self.outgoing_connections.iterator();
-        while (out_iter.next()) |entry| {
-            // TODO: Use a pool for the close context to avoid frequent allocations.
-            const close_ctx = self.allocator.create(ConnectionCloseCallbackCtx) catch unreachable;
-            close_ctx.* = ConnectionCloseCallbackCtx{
-                .notify = .{},
-                .@"switch" = self,
-            };
-            entry.value_ptr.*.close(close_ctx, ConnectionCloseCallbackCtx.closeCallback); // Close the connection, which will trigger the close callback.
-            close_ctx.notify.wait();
-            self.allocator.destroy(close_ctx);
-        }
-
-        self.outgoing_connections.deinit();
-
-        // Close all incoming connections. The onIncomingConnectionClose callback
-        // will be triggered for each, handling removal and cleanup.
-        for (self.incoming_connections.items) |conn| {
-            const close_ctx = self.allocator.create(ConnectionCloseCallbackCtx) catch unreachable;
-            close_ctx.* = ConnectionCloseCallbackCtx{
-                .notify = .{},
-                .@"switch" = self,
-            };
-            conn.close(close_ctx, ConnectionCloseCallbackCtx.closeCallback); // Close the connection, which will trigger the close callback.
-            close_ctx.notify.wait(); // Wait for the close callback to run.
-            self.allocator.destroy(close_ctx); // Clean up the close context.
-        }
-
-        self.incoming_connections.deinit();
-
-        self.transport.deinit(); // Deinitialize the transport.
-        // Close all listeners and free their resources.
-        var listener_iter = self.listeners.iterator();
-        while (listener_iter.next()) |entry| {
-            var listener = entry.value_ptr.*;
-            listener.deinit(); // This should close the listener's engine.
-
-            if (listener.listen_callback_ctx) |ctx| {
-                const accept_ctx: *ListenCallbackCtx = @ptrCast(@alignCast(ctx));
-                self.allocator.destroy(accept_ctx);
-            }
-            self.allocator.free(entry.key_ptr.*);
-        }
-        self.listeners.deinit();
-
-        self.proto_handlers.deinit();
+        self.is_stopping = true;
+        self.outgoingConnectionCloseAndClean();
     }
-
-    const ConnectionCloseCallbackCtx = struct {
-        notify: std.Thread.ResetEvent,
-
-        @"switch": *Switch,
-
-        fn closeCallback(ctx: ?*anyopaque, _: anyerror!*quic.QuicConnection) void {
-            const self: *ConnectionCloseCallbackCtx = @ptrCast(@alignCast(ctx.?));
-            // Notify that the connection has been closed.
-            self.notify.set();
-        }
-    };
 
     const ListenCallbackCtx = struct {
         @"switch": *Switch,
@@ -222,68 +167,6 @@ pub const Switch = struct {
         }
     };
 
-    // const ConnectCallbackCtx = struct {
-    //     @"switch": *Switch,
-
-    //     protocols: []const []const u8,
-    //     // user-defined context for the callback
-    //     callback_ctx: ?*anyopaque,
-    //     // user-defined callback function
-    //     callback: *const fn (callback_ctx: ?*anyopaque, controller: anyerror!?*anyopaque) void,
-
-    //     notify: std.Thread.ResetEvent,
-
-    //     conn: *quic.QuicConnection,
-
-    //     fn connectCallback(ctx: ?*anyopaque, res: anyerror!*quic.QuicConnection) void {
-    //         const self: *ConnectCallbackCtx = @ptrCast(@alignCast(ctx.?));
-
-    //         self.conn = res catch |err| {
-    //             self.callback(self.callback_ctx, err);
-    //             return;
-    //         };
-
-    //         self.conn.close_ctx = .{
-    //             .callback = Switch.onOutgoingConnectionClose,
-    //             .callback_ctx = self.@"switch",
-    //             .active_callback = null,
-    //             .active_callback_ctx = null,
-    //         };
-
-    //         self.@"switch".outgoing_connections.put(
-    //             std.fmt.allocPrint(self.@"switch".allocator, "{}", .{self.conn.connect_ctx.?.address}) catch unreachable,
-    //             self.conn,
-    //         ) catch unreachable;
-    //         // Can't call newStream in the callback directly, it will cause a re-entrancy issue.
-    //         self.notify.set();
-    //     }
-
-    //     fn newStreamCallback(ctx: ?*anyopaque, res: anyerror!*quic.QuicStream) void {
-    //         const self: *ConnectCallbackCtx = @ptrCast(@alignCast(ctx.?));
-    //         const stream = res catch |err| {
-    //             self.callback(self.callback_ctx, err);
-    //             return;
-    //         };
-
-    //         // TODO: To use multistreams, we need to find the protocol handler for the stream.
-    //         // For now, we just use the first protocol handler.
-    //         self.@"switch".proto_handlers.items[0].onInitiatorStart(stream, self.callback_ctx, self.callback);
-
-    //         // `onInitiatorStart` should set the stream's protocol message handler.
-    //         stream.proto_msg_handler.onActivated(stream) catch |err| {
-    //             std.log.warn("Proto message handler failed with error: {any}. Closing stream {any}.", .{ err, stream });
-    //             self.callback(self.callback_ctx, err);
-
-    //             stream.close(null, struct {
-    //                 fn callback(_: ?*anyopaque, _: anyerror!*quic.QuicStream) void {
-    //                     // Handle stream close if needed.
-    //                 }
-    //             }.callback);
-    //             return;
-    //         };
-    //     }
-    // };
-
     pub fn newStream(
         self: *Switch,
         address: std.net.Address,
@@ -291,6 +174,10 @@ pub const Switch = struct {
         callback_ctx: ?*anyopaque,
         callback: *const fn (callback_ctx: ?*anyopaque, controller: anyerror!?*anyopaque) void,
     ) void {
+        if (self.is_stopping) {
+            callback(callback_ctx, error.SwitchStopped);
+            return;
+        }
         const address_str = std.fmt.allocPrint(self.allocator, "{}", .{address}) catch unreachable;
         defer self.allocator.free(address_str);
 
@@ -324,6 +211,9 @@ pub const Switch = struct {
         callback_ctx: ?*anyopaque,
         callback: *const fn (callback_ctx: ?*anyopaque, controller: anyerror!?*anyopaque) void,
     ) !void {
+        if (self.is_stopping) {
+            return error.SwitchStopped;
+        }
         const address_str = std.fmt.allocPrint(self.allocator, "{}", .{address}) catch unreachable;
 
         const gop = self.listeners.getOrPut(address_str) catch unreachable;
@@ -355,6 +245,11 @@ pub const Switch = struct {
     fn onOutgoingConnectionClose(ctx: ?*anyopaque, res: anyerror!*quic.QuicConnection) void {
         const self: *Switch = @ptrCast(@alignCast(ctx.?));
 
+        if (self.is_stopping) {
+            // If the switch is stopping, we do not need to handle the connection close.
+            return;
+        }
+
         const conn = res catch |err| {
             std.log.warn("Connection close callback failed with error: {any}", .{err});
             return;
@@ -383,6 +278,11 @@ pub const Switch = struct {
     fn onIncomingConnectionClose(ctx: ?*anyopaque, res: anyerror!*quic.QuicConnection) void {
         const self: *Switch = @ptrCast(@alignCast(ctx.?));
 
+        if (self.is_stopping) {
+            // If the switch is stopping, we do not need to handle the connection close.
+            return;
+        }
+
         const conn = res catch |err| {
             std.log.warn("Connection close callback failed with error: {any}", .{err});
             return;
@@ -402,5 +302,66 @@ pub const Switch = struct {
         // This code is reached if the connection was not found in the list, which might
         // indicate a logic error elsewhere, but is safe to ignore for now.
         std.log.warn("Incoming connection close callback invoked, but connection not found in the list.", .{});
+    }
+
+    fn outgoingConnectionCloseAndCleanCallback(ctx: ?*anyopaque, _: anyerror!*quic.QuicConnection) void {
+        const self: *Switch = @ptrCast(@alignCast(ctx.?));
+
+        // This callback is called when an outgoing connection is closed.
+        // We will remove it from the list and clean up resources.
+        self.outgoingConnectionCloseAndClean();
+    }
+
+    fn outgoingConnectionCloseAndClean(self: *Switch) void {
+        const outgoing_entry = self.outgoing_connections.pop();
+        if (outgoing_entry) |e| {
+            self.allocator.free(e.key);
+            e.value.close(self, outgoingConnectionCloseAndCleanCallback);
+        } else {
+            self.incomingConnectionCloseAndClean(); // Clean up resources if no outgoing connections are left.
+        }
+    }
+
+    fn incomingConnectionCloseAndCleanCallback(ctx: ?*anyopaque, _: anyerror!*quic.QuicConnection) void {
+        const self: *Switch = @ptrCast(@alignCast(ctx.?));
+
+        // This callback is called when an incoming connection is closed.
+        // We will remove it from the list and clean up resources.
+        self.incomingConnectionCloseAndClean();
+    }
+
+    fn incomingConnectionCloseAndClean(self: *Switch) void {
+        const incoming_entry = self.incoming_connections.pop();
+        if (incoming_entry) |conn| {
+            conn.close(self, incomingConnectionCloseAndCleanCallback);
+        } else {
+            self.cleanResources(); // Clean up resources if no incoming connections are left.
+        }
+    }
+
+    fn cleanResources(self: *Switch) void {
+        // Because the `doClose` function of the `QuicEngine` may schedule a timer task, when it runs, the engine
+        // may be freed by `deinit` of `QuicTransport` or `QuicListener`, so we need to wait for a short time.
+        // Give some time for the connections to close gracefully.
+        // **Note**: Is 100ms enough?
+        std.time.sleep(100 * std.time.ns_per_ms);
+        self.outgoing_connections.deinit();
+        self.incoming_connections.deinit();
+
+        self.transport.deinit();
+        var listener_iter = self.listeners.iterator();
+        while (listener_iter.next()) |entry| {
+            var listener = entry.value_ptr.*;
+            listener.deinit(); // This should close the listener's engine.
+
+            if (listener.listen_callback_ctx) |lctx| {
+                const accept_ctx: *ListenCallbackCtx = @ptrCast(@alignCast(lctx));
+                self.allocator.destroy(accept_ctx);
+            }
+            self.allocator.free(entry.key_ptr.*);
+        }
+        self.listeners.deinit();
+
+        self.proto_handlers.deinit();
     }
 };
